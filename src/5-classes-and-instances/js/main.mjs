ddc88fbefb855667f5e46502cd7d561beed4a02b
Jacek Kopecky <jacek@jacek.cz>
2021-01-21 12:09:58 +0000

    add classes examples

---
+++ src/5-classes-and-instances/js/main.mjs
@@ -0,0 +1,47 @@
+import * as drawing from './drawing-classes.mjs';
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext("2d");
+
+const BUBBLES = 20;
+const AVG_SIZE = 30;
+
+const shapes = [
+  new drawing.Line(200, 500, 400, 500),
+  new drawing.Line(400, 500, 400, 300),
+  new drawing.Line(200, 300, 200, 500),
+  new drawing.Line(170, 330, 300, 200),
+  new drawing.Line(430, 330, 300, 200),
+];
+
+for (let i = 0; i < BUBBLES; i += 1) {
+  const circle = new drawing.Circle(
+    Math.random() * canvas.width,
+    Math.random() * canvas.height,
+    Math.random() * AVG_SIZE + AVG_SIZE / 2,
+  );
+  circle.dx = Math.random() + 1;
+  circle.dy = Math.random() - 0.5;
+  shapes.push(circle);
+}
+
+function draw() {
+  for (const shape of shapes) {
+    shape.draw(c);
+  }
+}
+
+function animate() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  move();
+  draw();
+  requestAnimationFrame(animate);
+}
+
+function move() {
+  for (const shape of shapes) {
+    if (shape instanceof drawing.Circle) shape.move(c, shape.dx, shape.dy);
+  }
+}
+
+animate();
---
+++ src/8-events/js/drawing-classes.mjs
@@ -0,0 +1,64 @@
+export class Shape {
+  draw(c) { }
+  move(c, dx, dy) { }
+
+  toString() {
+    return 'a shape';
+  }
+
+  log() {
+    console.log(this);
+  }
+}
+
+export class Line extends Shape {
+  constructor(x1, y1, x2, y2) {
+    super();
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  draw(c) {
+    c.beginPath();
+    c.moveTo(this.x1, this.y1);
+    c.lineTo(this.x2, this.y2);
+    c.stroke();
+  }
+
+  toString() {
+    return `line from ${this.x1},${this.y1} to ${this.x2},${this.y2}`;
+  }
+}
+
+export class Circle extends Shape {
+  constructor(x, y, r) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  draw(c) {
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    c.stroke();
+  }
+
+  move(c, dx, dy) {
+    this.x += dx;
+    this.y += dy;
+
+    // check bounds
+    if (this.x > c.canvas.width + this.r) this.x = -this.r;
+    if (this.y > c.canvas.height + this.r) this.y = -this.r;
+    if (this.x < -this.r) this.x = c.canvas.width + this.r;
+    if (this.y < -this.r) this.y = c.canvas.height + this.r;
+  }
+
+  // what if we remove this?
+  toString() {
+    return `circle at ${this.x},${this.y} with radius ${this.r}`;
+  }
+}
